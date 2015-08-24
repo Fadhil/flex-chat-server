f3c253ed2e76b49a770d59402e86da3036877e3a
Fadhil Luqman <fadhil.luqman@gmail.com>
2015-08-24 23:01:41 +0800

    Move socket.io stuff out to lib/flexio.js

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// Socket.io
-var socketio = require('socket.io');
-var io = socketio();
-app.io = io;
-
-io.on('connection', function(socket){
-   console.log('a user connected');
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
