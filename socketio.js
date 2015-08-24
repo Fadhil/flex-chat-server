317a9d53d8204b8d691055660edf029383506eef
Fadhil Luqman <fadhil.luqman@gmail.com>
2015-08-24 22:34:43 +0800

    Move socket.io directives into app.js

--- socketio.js
+++
@@ -1,7 +0,0 @@
-
-exports.initializeSocketIo = function(io) {
-  console.log('the io thing: ',io);
-     io.on('connection', function(socket){
-         console.log('a user connected');
-     });
-}
